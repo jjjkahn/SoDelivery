09b8a87e1a916fe8d53ea37164da3d369ab6db01
jjjkahn <31192156+jjjkahn@users.noreply.github.com>
2020-11-22 00:26:08 -0500

    FirstCommit Startup page set up proyect is set up  Id to see if its a Boss or a driver are set up also

---
+++ soDeliverySolution/SoDelivery.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SoDelivery.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult StartUp()
+        {
+            return View();
+        }
+        public ActionResult Index(int Id)
+        {
+            
+            if(Id == 1)
+            {
+                ViewBag.Title = "Boss";
+            }
+            else
+            {
+                ViewBag.Title = "Deliverer";
+            }
+            ViewBag.Id = Id;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ soDeliverySolution/SoDelivery.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoDelivery.WebUI.Startup))]
+namespace SoDelivery.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
